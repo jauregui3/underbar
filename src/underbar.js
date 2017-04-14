bc73063876e1b277fbdd8199ec9dfdd69502e4bf
Juancarlos Jauregui <jauregui3@gmail.com>
2017-04-14 15:15:37 -0700

    Add implementation for _.filter function

--- src/underbar.js
+++ src/underbar.js
@@ -77,6 +77,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var result = [];
+
+    _.each(collection, function(item) {
+      if (test(item) === true) {
+        result.push(item);
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.

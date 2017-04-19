c828cba4c6394a943134459f2c3e660b96bf2dcb
Juancarlos Jauregui <jauregui3@gmail.com>
2017-04-19 16:52:37 -0700

    Add implementation for _.invoke and _.sortBy

--- src/underbar.js
+++ src/underbar.js
@@ -353,6 +353,13 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
+    return _.map(collection, function(item) {
+      var method = functionOrKey;
+      if (typeof functionOrKey === 'string') {
+        method = item[functionOrKey];
+      }
+      return method.apply(item, args);
+    })
   };
 
   // Sort the object's values by a criterion produced by an iterator.
@@ -360,6 +367,15 @@
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
   _.sortBy = function(collection, iterator) {
+    if (typeof iterator === 'string') {
+      return collection.sort(function(a, b) {
+        return a[iterator] - b[iterator];
+      });
+    } else {
+      return collection.sort(function(a, b) {
+        return iterator(a) - iterator(b);
+      })
+    }
   };
 
   // Zip together two or more arrays with elements of the same index
